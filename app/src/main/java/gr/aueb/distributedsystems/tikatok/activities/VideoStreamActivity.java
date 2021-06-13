135c5162a02d62b1fd5a7517936d2b00ba9d8d15
Ana <anna.lesi2000@gmail.com>
2021-06-13 20:29:25 +0300

    added VideoStreamActivity

---
+++ app/src/main/java/gr/aueb/distributedsystems/tikatok/activities/VideoStreamActivity.java
@@ -0,0 +1,16 @@
+package gr.aueb.distributedsystems.tikatok.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import gr.aueb.distributedsystems.tikatok.R;
+
+public class VideoStreamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_stream);
+    }
+}
